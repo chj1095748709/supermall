7d3e58a8f081078c3b0a3ab1d15b82c02c112d33
chj1095748709 <1095748709@qq.com>
2021-01-21 17:01:51 +0800

    目录创建

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+//配置路由相关信息
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+import { cosh } from 'core-js/fn/number'
+
+//1.通过Vue.use(插件)，安装插件
+Vue.use(VueRouter)
+// 2.创建VueRouter对象
+const routes=[
+
+]
+export default router=new VueRouter({
+    //配置路由和组件之间的应用关系
+    routes
+})
